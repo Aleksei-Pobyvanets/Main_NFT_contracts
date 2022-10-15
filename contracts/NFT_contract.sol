20cc4a286a8210bb5c89483293c078bc356c07fb
Aleksei <kilroygood@gmail.com>
2022-10-15 15:18:23 +0300

    add functions

--- contracts/NFT_contract.sol
+++ contracts/NFT_contract.sol
@@ -21,7 +21,7 @@ contract NFT is ERC721Enumerable, Ownable {
     bool public revealed = false;
     bool public onlyWhitelisted = true;
 
-    address[] public ehitelistedAddresses;
+    address[] public whitelistedAddresses;
     mapping(address => uint) public addressMintedBalance;
 
 
@@ -47,8 +47,38 @@ contract NFT is ERC721Enumerable, Ownable {
         require(_mintAmount > 0, "You need to mint at least 1 NFT");
         require(_mintAmount <= maxMintAmount, "Max mint amount per session exceeded");
         require(supply + _mintAmount <= maxSupply, "Max FT limit exceeded ");
+   
+    if(msg.sender != owner()){
+        if(onlyWhitelisted == true){
+            require(isWhitelisted(msg.sender), "User is not whitelisted");
+            uint ownerMintedCount = addressMintedBalance[msg.sender];
+            require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "Max NFT per addrress exceeded");
+        }
+        require(msg.value >= cost * _mintAmount, "insufficient funds");
+    }
+    
+    for(uint i = 1; 1 <= _mintAmount; i++){
+        addressMintedBalance[msg.sender]++;
+        _safeMint(msg.sender, supply+1);
+    }
+   }
+
+    function isWhitelisted(address _user) public view returns(bool){
+        for(uint i = 0; i< whitelistedAddresses.length; i++){
+            if(whitelistedAddresses[i] == _user){
+                return true;
+            }
+        }
+        return false;
     }
 
-
+    function walletOfOwner(address _owner) public view returns(uint[] memory){
+        uint ownerTokenCount = balanceOf(_owner);
+        uint[] memory tokenIds = new uint[](ownerTokenCount);
+        for(uint i; i < ownerTokenCount; i++){
+            tokenIds[i] = tokenOfOwnerByIndex(_owner,i);
+        }
+        return tokenIds;
+    }
 
 }
