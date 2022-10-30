4fa12247c8c5362c50312f67c95676ee707511a2
Aleksei <kilroygood@gmail.com>
2022-10-31 01:50:30 +0200

    add

--- contracts/NFT_contract.sol
+++ contracts/NFT_contract.sol
@@ -71,13 +71,10 @@ contract NFT is ERC721Enumerable, Ownable {
     return false;
   }
 
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
+  function walletOfOwner(address _owner) public view returns (uint256[] memory){
     uint256 ownerTokenCount = balanceOf(_owner);
     uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    
     for (uint256 i; i < ownerTokenCount; i++) {
       tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
     }
--- test/Token.js
+++ test/Token.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 // const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers } = require("hardhat");
 const { waffle } = require("hardhat");
-const { deployContract } = waffle;
+// const { deployContract } = waffle;
 
 // describe("NFT contract", function(){
 
@@ -10,8 +10,6 @@ const { deployContract } = waffle;
 //         const myContract = await hre.ethers.getContractFactory("NFT");
 //         const myContractDeployed = await myContract.deploy("MyContractName", "TNT", "https://fdf",  "https://fdf");
 //         await myContractDeployed.deployed();
-
-//         // expect(await myContract.name()).to.equal("MyContractName")
 //     })
 //     it("should be deployed with right name", async function(){
 //         expect(await myContractDeployed.name()).to.equal("MyContractName");
@@ -28,8 +26,6 @@ describe("NFT contract", function(){
         const NFT = await ethers.getContractFactory("NFT", acc1);
         nft = await NFT.deploy("MyContractName", "TNT", "https://fdf123",  "https://fdf456");
         await nft.deployed();
-        console.log(nft.reveal(), "console log");
-        console.log(nft.address, acc1.address);
     })
 
     it("should be deployed with right name", async function(){
@@ -38,31 +34,50 @@ describe("NFT contract", function(){
     it("should be deployed with right symbol", async function(){
         expect(await nft.symbol()).to.equal("TNT");
     })
-    it("should return false", async function(){
-        expect(await nft.reveal().to.equal(true));
+    it("setmaxMintAmount", async function(){
+        await nft.setmaxMintAmount(2);
+        expect(await nft.maxMintAmount()).to.equal(2);
+    })
+    
+
+    describe("Mint", function(){
+        it("owner mint with add to addressMintedBalance", async function(){
+            let addrOwner = acc1.address
+            await nft.mint(1);
+            expect(await nft.addressMintedBalance(addrOwner)).to.equal(1);
+        })
+        it("not an owner mint with add to addressMintedBalance", async function(){
+            let addrNotAnOwner = acc2.address;
+            await nft.onlyWhitelisted(true);
+            await nft.isWhitelisted(true);
+            await nft.connect(acc2).mint(1);
+            expect(await nft.connect(acc2).addressMintedBalance(addrNotAnOwner)).to.equal(1);
+        })
     })
     
 })
 
 
 // describe("NFT contract", function(){
-//     async function deployNFTFixture(){
-//         const NFT = await ethers.getContractFactory("NFT");
-//         const [owner, addr1, addr2] = await ethers.getSigners();
 
-//         const hardhatNFT = await NFT.deploy("MyContractName", "TNT", "https://fdf",  "https://fdf");
-//         await hardhatNFT.deployed();
+//     let NFT, NFTContract, owner, addr1, addr2, addr3
 
-//         return{NFT, hardhatNFT, owner, addr1, addr2};
-//     }
+//     beforeEach(async function() {
+//     NFT = await ethers.getContractFactory("NFT");
+//     let [owner, addr1, addr2, addr3] = await ethers.getSigners();
+//     NFTContract = await NFT.deploy("MyContractName", "TNT", "https://fdf",  "https://fdf");
+//     await NFTContract.deployed();
+//     })
 
-//     describe("Deployment", function(){
-//         it("Should set the right owner", async function(){
-//             const {hardhatNFT, owner} = await loadFixture(deployNFTFixture);
-//             expect(await hardhatNFT.owner().to.equal(owner.address));
-//         });
+    // describe("deploy", function(){
+    //     it("should be owner", async function(){
+    //         expect(await NFTContract.owner()).to.equal(owner.address);
+    //     })
+    // })
 
-
-
-//     });
+//     describe("reveal", function(){
+//         it("should be owner reveal", async function(){
+//             expect(await NFTContract.connect(addr1).reveal(false)).to.be.revertedWith("You are not an owner");
+//         })
+//     })
 // })
