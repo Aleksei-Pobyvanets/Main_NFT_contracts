c284e026d704f73e955f917fd6f683115787cf4b
Aleksei <kilroygood@gmail.com>
2022-11-03 18:58:55 +0200

    add

--- test/Token.js
+++ test/Token.js
@@ -38,6 +38,16 @@ describe("NFT contract", function(){
         await nft.setmaxMintAmount(2);
         expect(await nft.maxMintAmount()).to.equal(2);
     })
+    it("paused false", async function(){
+        expect(await nft.paused()).to.equal(false);
+    })
+    it("revealed false", async function(){
+        expect(await nft.revealed()).to.equal(false);
+    })
+    it("onlyWhitelisted false", async function(){
+        expect(await nft.onlyWhitelisted()).to.equal(true);
+    })
+    
     
 
     describe("Mint", function(){
@@ -46,38 +56,13 @@ describe("NFT contract", function(){
             await nft.mint(1);
             expect(await nft.addressMintedBalance(addrOwner)).to.equal(1);
         })
-        it("not an owner mint with add to addressMintedBalance", async function(){
+        it("not an owner and without wl try to mint", async function(){
             let addrNotAnOwner = acc2.address;
-            await nft.onlyWhitelisted(true);
-            await nft.isWhitelisted(true);
-            await nft.connect(acc2).mint(1);
-            expect(await nft.connect(acc2).addressMintedBalance(addrNotAnOwner)).to.equal(1);
+            // await nft.onlyWhitelisted(true);
+            // await nft.isWhitelisted(true);
+            // await nft.connect(acc2).mint(1);
+            expect(await nft.connect(acc2).addressMintedBalance(addrNotAnOwner)).to.be.revertedWith("not an owner"); //be.revertedWith("not an owner");
         })
     })
     
 })
-
-
-// describe("NFT contract", function(){
-
-//     let NFT, NFTContract, owner, addr1, addr2, addr3
-
-//     beforeEach(async function() {
-//     NFT = await ethers.getContractFactory("NFT");
-//     let [owner, addr1, addr2, addr3] = await ethers.getSigners();
-//     NFTContract = await NFT.deploy("MyContractName", "TNT", "https://fdf",  "https://fdf");
-//     await NFTContract.deployed();
-//     })
-
-    // describe("deploy", function(){
-    //     it("should be owner", async function(){
-    //         expect(await NFTContract.owner()).to.equal(owner.address);
-    //     })
-    // })
-
-//     describe("reveal", function(){
-//         it("should be owner reveal", async function(){
-//             expect(await NFTContract.connect(addr1).reveal(false)).to.be.revertedWith("You are not an owner");
-//         })
-//     })
-// })
